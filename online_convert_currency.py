bd8f09f82a0564f531470d294d323182694634b3
Timchenko <tbd2504@gmail.com>
2022-07-04 18:51:55 +0300

    fifth commit

--- online_convert_currency.py
+++ online_convert_currency.py
@@ -7,8 +7,21 @@ import requests
 from tabulate import tabulate
 
 
-def check_correct_currency(currency: str, currencies: dict, default_currency: str):
-    currency = currency
+def load_symbols_json() -> dict:
+    url = 'https://api.exchangerate.host/symbols'
+    try:
+        currencies = load_file_json('symbols.json')
+    except FileNotFoundError:
+        response_url_2 = get_response_json(url, parameters={})
+        currencies = {'symbols': []}
+        for currency in response_url_2['symbols']:
+            currencies['symbols'].append(currency)
+        save_file_json('symbols.json', currencies)
+    return currencies
+
+
+def check_correct_currency(currency: str, currencies: dict, default_currency: str) -> str:
+    currency = currency.upper()
     for curr in currencies['symbols']:
         if currency == curr:
             return currency
@@ -19,7 +32,7 @@ def get_response_json(url: str, parameters={}) -> json:
     return requests.get(url, params=parameters).json()
 
 
-def valid_date(date: str) -> bool:
+def is_valid_date(date: str) -> bool:
     try:
         datetime.strptime(date, '%Y-%m-%d')
         return True
@@ -38,29 +51,28 @@ def comparison_dates(date_now: datetime, date_input: datetime) -> bool:
 def create_dates_list(date: str) -> list:
     ret = []
     date_now = datetime.now()
-    if valid_date(date):
+    if is_valid_date(date):
         date_input = datetime(*[int(elem) for elem in date.split('-')])
-
         if comparison_dates(date_now, date_input):
             while date_input <= date_now:
                 ret.append(date_input.strftime("%Y-%m-%d"))
                 date_input += timedelta(days=1)
         else:
             ret.append(date_now.strftime("%Y-%m-%d"))
-
     return ret
 
 
-def get_create_row_tab(url: str, date: str, currency_from: str, currency_to: str, amount: float) -> list:
+def get_create_row_tab(date: str, currency_from: str, currency_to: str, amount: float) -> list:
+    url = 'https://api.exchangerate.host/convert'
     parameters: dict[str, str | float] = {'date': date, 'from': currency_from, 'to': currency_to, 'amount': amount}
     response = get_response_json(url, parameters)
     return [date, currency_from, currency_to, amount, response['info']['rate'], response['result']]
 
 
-def get_create_tab(url: str, dates: list, currency_from: str, currency_to: str, amount: float) -> list:
+def get_create_tab(dates: list, currency_from: str, currency_to: str, amount: float) -> list:
     ret = [['date', 'from', 'to', 'amount', 'rate', 'result']]
     for date in dates:
-        ret.append(get_create_row_tab(url, date, currency_from, currency_to, amount))
+        ret.append(get_create_row_tab(date, currency_from, currency_to, amount))
     return ret
 
 
@@ -86,39 +98,30 @@ def load_file_json(file_name: str) -> dict:
 
 
 def get_args():
-    parser = ArgumentParser()
+    parser = ArgumentParser(prog='Онлайн конвертер валют')
     parser.add_argument('-from', '--currency_from', default='USD',
-                        help='Валюта яку конвертують.')
+                        help='Валюта яку конвертують. За замовчуванням USD.')
     parser.add_argument('-to', '--currency_to', default='UAH',
-                        help='Валюта в яку конвертують.')
-    parser.add_argument('--amount', type=float, default=100.00,
-                        help='Кількість грошей якую треба конвертувати.')
+                        help='Валюта в яку конвертують. За замовчуванням UAH.')
+    parser.add_argument('-a', '--amount', type=float, default=100.00,
+                        help='Кількість грошей якую треба конвертувати. За замовчуванням 100.00')
     parser.add_argument('--start_date', default=datetime.now().strftime('%Y-%m-%d'),
-                        help='Дата у форматі YYYY-MM-DD')
+                        help=f'Дата у форматі YYYY-MM-DD. За замовчуванням {datetime.now().strftime("%Y-%m-%d")}.')
 
-    parser.add_argument('--save_file', action='store_true')
+    parser.add_argument('--save_file', action='store_true',
+                        help='Зберігти відповідь у файл.За замовчуванням видає у консоль.')
     return parser.parse_args()
 
 
 def main():
-    url_1 = 'https://api.exchangerate.host/convert'
-    url_2 = 'https://api.exchangerate.host/symbols'
-
-    try:
-        currencies = load_file_json('symbols.json')
-    except FileNotFoundError:
-        response_url_2 = get_response_json(url_2, parameters={})
-        currencies = {'symbols': []}
-        for currency in response_url_2['symbols']:
-            currencies['symbols'].append(currency)
-        save_file_json('symbols.json', currencies)
+    currencies_list = load_symbols_json()
 
     args = get_args()
-    args.currency_from = check_correct_currency(args.currency_from, currencies, 'USD')
-    args.currency_to = check_correct_currency(args.currency_to, currencies, 'UAH')
+    args.currency_from = check_correct_currency(args.currency_from, currencies_list, 'USD')
+    args.currency_to = check_correct_currency(args.currency_to, currencies_list, 'UAH')
     dates = create_dates_list(args.start_date)
 
-    tab = get_create_tab(url_1, dates, args.currency_from, args.currency_to, args.amount)
+    tab = get_create_tab(dates, args.currency_from, args.currency_to, args.amount)
 
     if args.save_file:
         file_name = get_file_name(args.currency_from, args.curryncy_to, args.amount)
